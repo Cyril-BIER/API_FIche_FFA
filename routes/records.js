5d9d6c3d571e0f9bfc742840f05b8e307049fc94
Cyril <cyril.bier42100@gmail.com>
2024-06-23 22:37:29 +0200

    Get records by Licence Number

--- routes/records.js
+++ routes/records.js
@@ -1,19 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
-const getPB = require('../scrapping/athletes')
+const {getPB, getPBbyLicenceNumber} = require('../scrapping/athletes')
 
-
-
-
-router.get('/:identifiant', async function (req, res, next) {
+router.get('/', async function (req, res) {
     try {
-        const scrapedData = await getPB(req.params.identifiant);
+        let scrapedData
+        if(req.query.id != null) {
+            scrapedData = await getPB(req.query.id);
+        }else{
+            if(req.query.licence != null){
+                scrapedData = await getPBbyLicenceNumber(req.query.licence);
+            }else{
+                scrapedData = "Veuillez renseigner le champ id ou licence"
+            }
+        }
         res.json(scrapedData);
     } catch (error) {
         console.error(error);
         res.status(500).send('Error occurred while scraping data');
     }
 });
+
 
 module.exports = router;
--- scrapping/athletes.js
+++ scrapping/athletes.js
@@ -6,6 +6,34 @@ const puppeteer = require('puppeteer');
 const strToHex = require('../utils/stringTohex')
 
 /**
+ * Allows to get the DB ID of the athlete corresponding to the licence number given in parameter 
+ * @param {*} licenceNumber 
+ */
+async function getId(licenceNumber) {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    let year = new Date().getFullYear();
+    let id = null;
+    while (id == null && year >= 2004) {
+        await page.goto(`https://bases.athle.fr/asp.net/liste.aspx?frmpostback=true&frmbase=resultats&frmmode=1&frmespace=0&frmsaison=${year}&frmlicence=${licenceNumber}`)
+
+        id = await page.evaluate(() => {
+            const link = document.querySelector('a[href*="bddThrowAthlete"]');
+
+            if (link) {
+                const href = link.getAttribute('href');
+                const match = href.match(/bddThrowAthlete\('resultats',\s*(\d+),\s*\d+\)/);
+                return match ? match[1] : null;
+            }
+            return null;
+        });
+        year = year - 1;
+    }
+
+    return id;
+}
+
+/**
  * Allows to get PBs of the athlete provided in parameter
  * @param {*} id 
  * @returns 
@@ -19,7 +47,7 @@ async function getPB(id) {
         // Select the first table within #divRecord
         const table = document.querySelector('#divRecord table');
         if (!table) return []; // Return an empty array if no table is found
-        
+
         const rows = Array.from(table.querySelectorAll('tr'));
         // Skip the first row if it is a header row
         return rows.slice(1).map(row => {
@@ -43,4 +71,18 @@ async function getPB(id) {
     return data;
 }
 
-module.exports= getPB;
+/**
+ * Allows to get PBs of the athlete corresponding to the licence number provided in parameter
+ * @param {*} licenceNumber 
+ */
+async function getPBbyLicenceNumber(licenceNumber) {
+    const id = await getId(licenceNumber)
+    if (id == null ) return "Aucune donnée trouvée pour ce numéro de licence"
+    return await getPB(id)
+}
+
+module.exports = {
+    getId,
+    getPB,
+    getPBbyLicenceNumber
+}
